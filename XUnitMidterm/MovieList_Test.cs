2ec3113f0102ff37f6995e39e58b228afd406dd2
Socrates Katrivesis <socrateskatrivesis@quickenloans.com>
2020-02-25 13:43:54 -0500

    code and unit test refactor

--- XUnitMidterm/MovieList_Test.cs
+++ XUnitMidterm/MovieList_Test.cs
@@ -9,7 +9,7 @@ namespace XUnitMidterm
     public class MovieList_Test
     {
         [Fact]
-        public void Sort_Name_True()
+        public void Sort_Name_True() //sorts are all the same except for different fields
         {
             MovieList movieList = new MovieList();
             List<Movie> movies = new List<Movie>();
@@ -23,6 +23,34 @@ namespace XUnitMidterm
             movies = movieList.ReturnMovieList();
 
             Assert.Equal("Die Hard", movies[0].MovieName);
+        }
+
+        [Fact]
+        public void Movie_SortGenre_True() 
+        {
+            MovieList movieList = new MovieList();
+            List<Movie> movies = new List<Movie>();
+
+            movieList.InitializeMovieList();
+
+            movieList.MovieSortByActor();
+
+            movies = movieList.ReturnMovieList();
+
+            Assert.Equal("Brad Pitt", movies[0].MainActor);
+        }
+        
+        [Fact]
+        public void Movie_Initialize_Count()
+        {
+            MovieList movieList = new MovieList();
+            List<Movie> movies = new List<Movie>();
+
+            movieList.InitializeMovieList(); //9 movies are initialized as of now. 
+
+            movies = movieList.ReturnMovieList();
+
+            Assert.Equal(9, movies.Count);
         }
     }
 }
